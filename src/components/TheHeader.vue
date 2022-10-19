745abc23ac3e3a9909be080502ac2acbea8095bf
MrNedNick <mrnediala@gmail.com>
2022-10-19 10:27:32 +0200

    Initial commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header-wrapper">
+    <div class="logo-wrapper">
+      
+    </div>
+    <div class="nav-wrapper">
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <the-header />
+  </div>
+</template>
+
+<script>
+import TheHeader from '../components/TheHeader.vue'
+export default {
+  components: { TheHeader },
+
+}
+</script>
+
+<style>
+
+</style>
