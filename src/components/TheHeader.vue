0f6c5e2d561c24ade701d9e68ecb7a64ba62c067
MrNedNick <mrnediala@gmail.com>
2022-10-19 10:39:00 +0200

    Add navigation

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,7 +4,13 @@
       <img src="@/assets/logo.png" alt="Logo">
     </div>
     <div class="nav-wrapper">
-
+      <nav>
+        <a href="" class="nav-item">Home</a>
+        <a href="" class="nav-item">Aanbod</a>
+        <a href="" class="nav-item">Reparatie</a>
+        <a href="" class="nav-item">Inruilen</a>
+      </nav>
+      <button class="nav-button">Neem contact op</button>
     </div>
   </header>
 </template>
